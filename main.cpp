3c6e29f8a6d78017512bcfd1be5e0ece287fa77b
cryscan <cryscan@umich.edu>
2020-10-18 11:54:39 -0400

    Compete functions.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 #include <vector>
 #include <string>
 #include <map>
-#include <tuple>
+#include <random>
+#include <cassert>
 
+typedef float auto;
 using std::cout;
 using std::endl;
 using std::vector;
 using std::string;
 using std::map;
 using std::tuple;
+
+static std::default_random_engine generator;
 
 vector<string> read_names(const string& filename) {
     vector<string> names;
@@ -30,6 +35,7 @@ enum class Card {
 };
 
 struct Actor {
+    int id;
     string name;
 
     int stone_count;
@@ -38,7 +44,11 @@ struct Actor {
 
     int star_count;
 
+    bool operator==(const Actor& other) const { return id == other.id; }
+
     [[nodiscard]] int total_count() const { return stone_count + scissor_count + paper_count; }
+
+    [[nodiscard]] bool can_compete() const { return star_count > 0 || scissor_count > 0 || paper_count > 0; }
 };
 
 struct Global {
@@ -46,23 +56,25 @@ struct Global {
     int scissor_count;
     int paper_count;
 
-    int actor_count;
+    vector<Actor> actors;
 
     [[nodiscard]] int total_count() const { return stone_count + scissor_count + paper_count; }
 };
 
-tuple<vector<Actor>, Global> init_actors_global(int total_count, const vector<string>& names) {
+Global init_global(int total_count, const vector<string>& names) {
     vector<Actor> actors(total_count);
-    Global global{total_count, total_count, total_count, total_count};
+    Global global{total_count, total_count, total_count};
 
     for (int i = 0; i < total_count; ++i) {
         auto& actor = actors[i];
+        actor.id = i + 1;
         actor.name = names[i];
         actor.paper_count = actor.scissor_count = actor.stone_count = 1;
         actor.star_count = 3;
     }
 
-    return std::tie(actors, global);
+    global.actors = std::move(actors);
+    return global;
 }
 
 void consume_card(Global& global, Actor& actor, Card card) {
@@ -82,6 +94,14 @@ void consume_card(Global& global, Actor& actor, Card card) {
     }
 }
 
+void check_actor(Global& global, const Actor& actor) {
+    if (actor.star_count <= 0) {
+        auto& actors = global.actors;
+        auto iter = std::find(actors.begin(), actors.end(), actor);
+        actors.erase(iter);
+    }
+}
+
 map<Card, float> competitor_prob(const Global& global, const Actor& actor) {
     int total_count = global.total_count() - actor.total_count();
     map<Card, float> prob;
@@ -91,9 +111,38 @@ map<Card, float> competitor_prob(const Global& global, const Actor& actor) {
     return prob;
 }
 
+Card actor_compete(const Global& global, const Actor& actor) {
+    assert(actor.can_compete());
+    auto prob = competitor_prob(global, actor);
+
+    float sum = 0;
+    if (actor.paper_count > 0) sum += prob[Card::STONE];
+    if (actor.stone_count > 0) sum += prob[Card::SCISSOR];
+    if (actor.scissor_count > 0) sum += prob[Card::PAPER];
+
+    auto dist = std::uniform_real_distribution<float>(0, sum);
+    auto rand = dist(generator);
+
+    sum = 0;
+    if (actor.paper_count > 0) {
+        sum += prob[Card::STONE];
+        if (sum > rand) return Card::PAPER;
+    }
+    if (actor.stone_count > 0) {
+        sum += prob[Card::SCISSOR];
+        if (sum > rand) return Card::STONE;
+    }
+    if (actor.scissor_count > 0) return Card::SCISSOR;
+}
+
+
+
 int main() {
     auto names = read_names("names.txt");
-    auto[actors, global] = init_actors_global(100, names);
+    auto global = init_global(100, names);
+
+    Actor player{0, "Player", 1, 1, 1, 3};
+
 
     return 0;
 }
